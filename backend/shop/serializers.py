0d46ba47785e6e9a12dc44ba4cdfac816d6c7488
Mahdi <savehmahdi1383@gmail.com>
2025-04-08 12:57:48 +0330

    drf, serializers added

---
+++ backend/shop/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Product, Discount, OrderItem, Order
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class DiscountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discount
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    item = OrderItemSerializer(many=True)
+    class Meta:
+        model = Order
+        fields = '__all__'
